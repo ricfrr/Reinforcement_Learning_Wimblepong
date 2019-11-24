99d5a3d0895dbb18bc3e293d1864cc29cad39256
ricfrr <riccardo.franceschini@live.i>
2019-11-24 11:42:23 +0200

    some improvements in actor critic implementation

--- Agent.py
+++ Agent.py
@@ -3,6 +3,12 @@ import random
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.distributions import Categorical
+import cv2
+import numpy as np
+
+import matplotlib.pyplot as plt
+
 
 class PolicyConv(torch.nn.Module):
     def __init__(self, action_space, hidden=64):
@@ -10,25 +16,57 @@ class PolicyConv(torch.nn.Module):
         self.action_space = action_space
         self.hidden = hidden
         self.conv1 = torch.nn.Conv2d(2, 32, 3, 2)
+        self.conv1_bn = nn.BatchNorm2d(32)
         self.conv2 = torch.nn.Conv2d(32, 64, 3, 2)
+        self.conv2_bn = nn.BatchNorm2d(64)
         self.conv3 = torch.nn.Conv2d(64, 128, 3, 2)
+        self.conv3_bn = nn.BatchNorm2d(128)
+        
+
         self.reshaped_size = 128*11*11
         self.fc1_actor = torch.nn.Linear(self.reshaped_size, self.hidden)
+        self.fc1_actor_bn = nn.BatchNorm1d(self.hidden)
         self.fc1_critic = torch.nn.Linear(self.reshaped_size, self.hidden)
+        self.fc1_critic_bn = nn.BatchNorm1d(self.hidden)
+        
         self.fc2_mean = torch.nn.Linear(self.hidden, action_space)
+        
         self.fc2_value = torch.nn.Linear(self.hidden, 1)
+        self.initialize()
+
+    def initialize(self):
+        torch.nn.init.xavier_uniform_(self.conv1.weight)
+        torch.nn.init.xavier_uniform_(self.conv2.weight)
+        torch.nn.init.xavier_uniform_(self.conv3.weight)
+        torch.nn.init.xavier_uniform_(self.fc1_actor.weight)
+        torch.nn.init.xavier_uniform_(self.fc1_critic.weight)
+        torch.nn.init.xavier_uniform_(self.fc2_mean.weight)
+        torch.nn.init.xavier_uniform_(self.fc2_value.weight)
+
 
     def forward(self, x):
         x = self.conv1(x)
         x = F.relu(x)
+        x = self.conv1_bn(x)
+        x = F.relu(x)
         x = self.conv2(x)
         x = F.relu(x)
+        x = self.conv2_bn(x)
+        x = F.relu(x)
+
         x = self.conv3(x)
+        x = F.relu(x)
+        x = self.conv3_bn(x)
+
         x = F.relu(x)
 
         x = x.reshape(-1, self.reshaped_size)
         x_ac = self.fc1_actor(x)
         x_ac = F.relu(x_ac)
+        x_ac = self.fc1_actor_bn(x_ac)
+        
+        x_ac = F.relu(x_ac)
+
         x_mean = self.fc2_mean(x_ac)
 
         x_probs = F.softmax(x_mean, dim=-1)
@@ -36,45 +74,12 @@ class PolicyConv(torch.nn.Module):
 
         x_cr = self.fc1_critic(x)
         x_cr = F.relu(x_cr)
+        x_cr = self.fc1_critic_bn(x_cr)
+        x_cr = F.relu(x_cr)
+
         value = self.fc2_value(x_cr)
 
         return dist, value
-
-
-
-class ActorCritic(torch.nn.Module):
-    def __init__(self, num_inputs):
-        super(ActorCritic, self).__init__()
-       
-        #convolutional part 
-        self.conv1 = nn.Conv2d(num_inputs, 32, 3)
-        self.conv2 = nn.Conv2d(32, 32, 3)
-        self.conv3 = nn.Conv2d(32, 32, 3)
-        self.conv4 = nn.Conv2d(32, 32, 3)
-        #end convolution
-
-        self.fc1 = nn.Linear(self.conv4.out_channels*3*3, 256)
-        self.lstm = nn.LSTMCell(32 * 3 * 3, 256)
-        num_outputs = 3
-        #critic
-        self.critic_linear = nn.Linear(256, 1)
-        #actor
-        self.actor_linear = nn.Linear(256, num_outputs)
-        self.pool = nn.MaxPool2d(2, 2)
-
-    def forward(self, inp):
-        inp = inp.view(1,1,100,100)
-        inp = inp.float()
-
-        x = self.pool(F.relu(self.conv1(inp)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = self.pool(self.pool(F.relu(self.conv3(x))))
-        x = F.relu(self.conv4(x))
-        x = x.view(x.size(0), -1)
-        #print(x.shape)
-        x = self.fc1(x)
-        
-        return self.critic_linear(x), self.actor_linear(x)
 
 
 
@@ -86,17 +91,20 @@ class Agent(object):
         self.env = env
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-        # Set the player id that determines on which side the ai is going to play
-        self.actor_critic = ActorCritic(num_inputs)
-        self.actor_critic = self.actor_critic.to(self.device)
+
+        self.policy = PolicyConv(3, 128).to(self.device)
+        #second targt net 
+        self.target_net = PolicyConv(3, 128).to(self.device)
+        self.target_net.load_state_dict(self.policy.state_dict())
+        self.target_net.eval()
+
+        self.prev_obs = None
+
         #self.optimizer = optim.Adam(shared_model.parameters(), lr=args.lr)
         self.player_id = player_id  
-        # Ball prediction error, introduce noise such that SimpleAI reflects not
-        # only in straight lines
-        self.bpe = 4               
-        self.name = "Maialo"
+        self.name = "Bro"
        
-        self.optimizer = torch.optim.Adam(self.actor_critic.parameters(), lr=5e-3)
+        self.optimizer = torch.optim.RMSprop(self.policy.parameters(), lr=5e-3)
         self.gamma = 0.98
         
         self.states = []
@@ -105,21 +113,21 @@ class Agent(object):
         self.values = [] # values saved during the training 
 
     def update(self):
-        action_probs = torch.stack(self.action_probs, dim=0) \
-                .to(self.device).squeeze(-1)
+        action_probs = torch.stack(self.action_probs, dim=0).to(self.device).squeeze(-1)
         rewards = torch.stack(self.rewards, dim=0).to(self.device).squeeze(-1)
         values = torch.stack(self.values, dim=0).to(self.device).squeeze(-1) # values from the network
         self.states, self.action_probs, self.rewards, self.values = [], [], [], []
 
         
-        discounted_rewards = self.discount_rewards(rewards,self.gamma) # TASK 3
+        discounted_rewards = self.discount_rewards(rewards,self.gamma)
         
         advantage =  discounted_rewards - values 
         advantage -= torch.mean(advantage)
         advantage /= torch.std(advantage.detach())
 
-        weighted_probs = -action_probs* advantage.detach() 
-
+        action_probs = action_probs.view(action_probs.shape[0],action_probs.shape[2] ).T
+        weighted_probs = -action_probs[0]* advantage.detach()
+        
 
         actor_loss = weighted_probs.mean()
         critic_loss = advantage.pow(2).mean()
@@ -132,28 +140,16 @@ class Agent(object):
         self.optimizer.step()
         self.optimizer.zero_grad()
 
-
-
-    def get_action(self, state, epsilon=0.05):
-        state = torch.tensor(state)
-        state.to(self.device)
-        value, policy = self.actor_critic.forward(state)
-        prob = F.softmax(policy, dim=-1)
-        log_prob = F.log_softmax(policy, dim=-1)
-        #entropy = -(log_prob * prob).sum(1, keepdim=True)
-        #entropies.append(entropy)
-
-        action = prob.multinomial(num_samples=1).detach()
-        log_prob = log_prob.gather(1, action)
-       
-        self.action_probs.append(log_prob)
+    def get_action(self, observation):
+        x = self.preprocess(observation).to(self.device)
+        dist, value = self.target_net.forward(x)
+        #dist, value = self.policy.forward(x)
+        action = torch.argmax(dist.probs)
         self.values.append(value)
+        self.action_probs.append(dist.probs)
         return action
 
     def get_name(self):
-        """
-        Interface function to retrieve the agents name
-        """
         return self.name
 
     def store_outcome(self, observation, action_taken, reward):
@@ -161,8 +157,24 @@ class Agent(object):
         self.rewards.append(torch.Tensor([reward]))
 
     def reset(self):
-        # Nothing to done for now...
-        return
+        self.prev_obs = None
+    
+    def preprocess(self, observation):
+        observation = np.array(observation[0])
+        observation = observation[::2, ::2].mean(axis=-1)
+
+        observation = np.expand_dims(observation, axis=-1)
+        if self.prev_obs is None:
+            self.prev_obs = observation
+        stack_ob = np.concatenate((self.prev_obs, observation), axis=-1)
+        stack_ob = torch.from_numpy(stack_ob).float().unsqueeze(0)
+        stack_ob = stack_ob.transpose(1, 3)
+        self.prev_obs = observation
+        return stack_ob
+    
+    def load_model(self):
+        weights = torch.load("model.mdl")
+        self.policy.load_state_dict(weights, strict=False)
 
     def discount_rewards(self,r, gamma):
         discounted_r = torch.zeros_like(r)
@@ -171,5 +183,8 @@ class Agent(object):
             running_add = running_add * gamma + r[t]
             discounted_r[t] = running_add
         return discounted_r
+
+    def update_target_network(self):
+        self.target_net.load_state_dict(self.policy.state_dict())
 
 
--- pong_main.py
+++ pong_main.py
@@ -12,7 +12,7 @@ import wimblepong
 from PIL import Image
 from Agent import Agent
 import cv2
-from utils import to_gray_scale_and_downsample
+import torch
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--headless", action="store_true", help="Run in headless mode")
@@ -36,37 +36,56 @@ player = Agent(env, 1, player_id) #Agent(env, player_id) #introduce here our age
 # Set the names for both SimpleAIs
 env.set_names(player.get_name(), opponent.get_name())
 matches =0
-win1 = 0
+points_1 = 0
+points_2 = 0
+point_history1 = []
+point_history2 = []
+timesteps_history = []
+
 for i in range(0,episodes):
-    if i%21==0:
-        print("-------Match",matches)
-        
-        matches +=1
-    done = False
+
     observation = env.reset()
-    observation = to_gray_scale_and_downsample(observation[0])
-    while not done:
-        action1 = player.get_action(observation) # our are all the 1 because we are the action 1 
-        action2 = opponent.get_action()
-        # Step the environment and get the rewards and new observations
-        (ob1, ob2), (rew1, rew2), done, info = env.step((action1, action2))
-        #convert to grayscale
-        ob1_g_d =  to_gray_scale_and_downsample(ob1)
-        player.store_outcome(ob1_g_d, action1, rew1) 
-        #img = Image.fromarray(ob1)
-        #img.save("ob1.png")
-        #img = Image.fromarray(ob2)
-        #img.save("ob2.png")
-        # Count the wins
-        if i%21==0:
-            win1 = 0
-        
-        if rew1 == 10:
-            win1 += 1
-        if not args.headless:
-            env.render()
-        if done:
-            observation= env.reset()
-            print("episode {} over. Broken WR: {:.3f}".format(i%21, win1/(i%21+1)))
+    #match loop
+    match_done = False
+    timesteps_list = []
+    while not match_done:
+        done = False
+        #point loop
+        timesteps = 0
+        while not done:
+            timesteps += 1
+            action1 = player.get_action(observation) # our are all the 1 because we are the action 1 
+            action2 = opponent.get_action()
+            # Step the environment and get the rewards and new observations
+            (ob1, ob2), (rew1, rew2), done, info = env.step((action1, action2))
+
+            player.store_outcome(ob1, action1, rew1) 
+
+            if not args.headless:
+                env.render()
+            if done:
+                if rew1 == 10:
+                    points_1 += 1
+                else:
+                    points_2 += 1
+                observation= env.reset()
+
+        timesteps_list.append(timesteps)
+        player.update()
+        if points_1 >= 21 or points_2 >=21:
+            match_done = True
+    print("episode " , i ," over. result = Bro : ",points_1, " AI : ",points_2, " avg_timesteps : ", np.array(timesteps_list).mean())
+    point_history1.append(int(points_1))
+    point_history2.append(int(points_2))
+    timesteps_history.append(np.array(timesteps_list).mean())
     
-    player.update()
+    points_1 =0
+    points_2 =0          
+    if i %100 ==0:
+        torch.save(player.policy.state_dict(),"weights_%s_%d.mdl" % ("bro", i)) 
+        # saving point made by bro
+        np.savetxt('point_history_1.txt', np.array(point_history1), delimiter=',')
+        np.savetxt('point_history_2.txt', np.array(point_history2), delimiter=',')
+        np.savetxt('timestep_history.txt', np.array(timesteps_history), delimiter=',')
+    
+    player.update_target_network()
